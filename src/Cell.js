5b5200155a01cc7a3b40cfd08ebd35528a229a3f
Alex Merecka <merecka@gmail.com>
2020-01-20 11:50:14 -0600

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Cell extends Component {
 
 	constructor(props) {
-		super()
-		this.state = { color: props.value }
+		super(props)
+		this.state = { color: this.props.value }
 	}
 
 	changeColor = () => {
